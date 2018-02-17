08ae4c7bc6b3daede485fbbeb714e7c167942c06
Stephen Henry <admin@stephenhenry.ie>
2018-02-17 16:44:49 +0000

    Changes for PEP 8

--- pyautogui_click.py
+++ pyautogui_click.py
@@ -7,12 +7,12 @@ upperLeft = list(position())
 lowerRight = list(position())
 
 while True:
-    pauseMax = randint(90,110)
-    pauseTime = randint(8,12)
-    for item in range(0,pauseMax):
+    pauseMax = randint(90, 110)
+    pauseTime = randint(8, 12)
+    for item in range(0, pauseMax):
         r = random()
-        x = randint(upperLeft[0],lowerRight[0])
-        y = randint(upperLeft[1],lowerRight[1])
-        pag.click(x,y)
+        x = randint(upperLeft[0], lowerRight[0])
+        y = randint(upperLeft[1], lowerRight[1])
+        position.click(x, y)
         time.sleep(r)
     time.sleep(pauseTime)
--- threadingframe.py
+++ threadingframe.py
@@ -4,11 +4,13 @@ import time
 
 task_lock = threading.Lock()
 
+
 def exampleJob(worker):
-    time.sleep(.5) # pretend to do some work.
+    time.sleep(.5)  # pretend to do some work.
     with task_lock:
-        print(threading.current_thread().name,worker)
-        
+        print(threading.current_thread().name, worker)
+
+
 # The threader thread pulls an worker from the queue and processes it
 def threader():
     while True:
@@ -18,17 +20,18 @@ def threader():
         exampleJob(worker)
         # completed with the job
         q.task_done()
-        
-# Create the queue and threader 
+
+
+# Create the queue and threader
 q = Queue()
 
 # how many threads are we going to allow for
 for x in range(10):
-     t = threading.Thread(target=threader)
-     # classifying as a daemon, so they will die when the main dies
-     t.daemon = True
-     # begins, must come after daemon definition
-     t.start()
+    t = threading.Thread(target=threader)
+    # classifying as a daemon, so they will die when the main dies
+    t.daemon = True
+    # begins, must come after daemon definition
+    t.start()
 
 start = time.time()
 
@@ -39,4 +42,4 @@ for worker in range(20):
 # wait until the thread terminates.
 q.join()
 
-print('Entire job took:',time.time() - start)
+print('Entire job took:', time.time() - start)
